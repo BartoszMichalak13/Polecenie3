5e38c5f6e1d99881cce03edb6953dce82cd7e297
Bartosz Michalak <michalak.bartosz67@gmail.com>
2022-10-20 20:08:56 +0200

    This is my 1st commit

---
+++ src/main/java/org/example/Fun.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Fun
+{
+    public int main(int a)
+    {
+        a=a*a;
+        return a;//moge zmienic to na a*a*a wrzucic na githuba, ktos pobierze a ja cofam commita, on napisze
+        //cos w oparciu o a*a*a i to wrzuci a program wypluwa a*a i jest problem
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        Fun function = new Fun();
+        int a=3;
+        System.out.println(function.main(a));
+    }
+}
